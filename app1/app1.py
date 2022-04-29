be50adbada33e8dbf4c904c25473953ba09d2b3a
Talha Ahsan <talha.ahsan@astera.com>
2022-04-30 03:51:23 +0500

    added files

--- app1/app1.py
+++ app1/app1.py
@@ -6,5 +6,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return 'Rizwan Nadeem Web App app 1!'
-app.run(host='0.0.0.0', port=5000)
+
+if __name__ == "__main__":
+
+  app.run(host='0.0.0.0', port=5000)
 
--- app2/app2.py
+++ app2/app2.py
@@ -1,11 +1,13 @@
 
-
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Rizwan Nadeem app2!'
+    return 'Rizwan Nadeem Web App app 1!'
 
-app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+
+  app.run(host='0.0.0.0', port=5000)
+
